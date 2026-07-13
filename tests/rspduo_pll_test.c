c1d1bfc0fee37e061b815d1ac721e32a45562f32
Ryan <git@ftnsa.com>
2026-07-13 03:25:03 -0400

    Test RSPduo six-megasample format word

--- tests/rspduo_pll_test.c
+++ tests/rspduo_pll_test.c
@@ -12,6 +12,8 @@ int main(void)
     assert(mirisdr_rspduo_pll_words(2048000u, &reg3, &reg4) == 0);
     assert(reg3 == 0x01181fu);
     assert(reg4 == 0x0624ddu);
+    assert(mirisdr_rspduo_252_format_word(2048000u) == 0x000005u);
+    assert(mirisdr_rspduo_252_format_word(6000000u) == 0x000094u);
     assert(mirisdr_rspduo_pll_words(0u, &reg3, &reg4) < 0);
     assert(mirisdr_rspduo_pll_words(2048000u, NULL, &reg4) < 0);
     puts("RSPDUO_PLL_OK");
